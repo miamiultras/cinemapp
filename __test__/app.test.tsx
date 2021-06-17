86de36578383df8af7820d05a16b33fad1ee72a5
miamiultras <zurawinaiserow@gmail.com>
2021-06-17 19:36:59 +0000

    fix app tests, add card tests

--- __test__/app.test.tsx
+++ __test__/app.test.tsx
@@ -1,16 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import App from "../pages/index";
+import { MOVIE_FIXTURE } from "./fixtures/movie";
 
 describe("App", () => {
   it("renders without crashing", () => {
-    render(<App />);
+    render(<App moviesData={[MOVIE_FIXTURE]} />);
     expect(
       screen.getByRole("heading", { name: "Cinem app" })
     ).toBeInTheDocument();
   });
 
   it("renders subtitle", () => {
-    render(<App />);
+    render(<App moviesData={[MOVIE_FIXTURE]} />);
     expect(screen.getByText("Reserve tickets easier")).toBeInTheDocument();
   });
 });
