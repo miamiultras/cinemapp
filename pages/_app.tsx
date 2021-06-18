8da37cd364c3cb983cd5c0018ab045d7d9528a0a
miamiultras <zurawinaiserow@gmail.com>
2021-06-18 21:40:28 +0000

    add transition page animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import "../styles/nprogress.css";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
